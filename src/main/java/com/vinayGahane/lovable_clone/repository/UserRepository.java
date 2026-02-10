c1540ffd684027c7f86338b4c510ff759a7b8c64
vinaygahane <56063400+vinaygahane@users.noreply.github.com>
2026-02-10 20:35:35 +0530

    added repositories

---
+++ src/main/java/com/vinayGahane/lovable_clone/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.vinayGahane.lovable_clone.repository;
+
+import com.vinayGahane.lovable_clone.entity.Project;
+import com.vinayGahane.lovable_clone.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
+
